e96f3ac9073428ba9004237954b416ad679a698a
luansantosdevpy <luansantosdev.py@gmail.com>
2024-07-23 21:55:20 -0300

    feat: initial testes to todoController and mock model

---
+++ tests/unit/todo.controller.test.js
@@ -0,0 +1,41 @@
+const todoController = require("../../controllers/todo.controller");
+const TodoModel = require("../../model/todo.model");
+const httpMocks = require("node-mocks-http");
+const newTodo = require("../mock-data/new-todo.json");
+
+TodoModel.create = jest.fn();
+
+let req, res, next;
+
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+})
+
+describe("TodoController.createTodo", () =>{
+
+    beforeEach(() => {
+        req.body = newTodo;
+    })
+    it("should have a createTodo function", () => {
+        expect(typeof todoController.createTodo).toBe("function");
+    });
+
+    it("should call TodoModel.create", () => {
+        todoController.createTodo(req, res, next);
+        expect(TodoModel.create).toBeCalledWith(newTodo);
+    });
+
+    it("should return 201 response code", () => {
+        todoController.createTodo(req, res, next);
+        expect(res.statusCode).toBe(201);
+        expect(res._isEndCalled()).toBeTruthy();
+    });
+
+    it("should return json body response", () => {
+        TodoModel.create.mockReturnValue(newTodo);
+        todoController.createTodo(req, res, next);
+        expect(res._getJSONData()).toStrictEqual(newTodo);
+    });
+})
