f59785fa5d7cbfa3af66eb003182bb26986d676b
luansantosdevpy <luansantosdev.py@gmail.com>
2024-07-24 12:58:44 -0300

    feat: create handle error to throw errors and tests

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -10,7 +10,7 @@ let req, res, next;
 beforeEach(() => {
     req = httpMocks.createRequest();
     res = httpMocks.createResponse();
-    next = null;
+    next = jest.fn();
 })
 
 describe("TodoController.createTodo", () =>{
@@ -38,4 +38,11 @@ describe("TodoController.createTodo", () =>{
         await todoController.createTodo(req, res, next);
         expect(res._getJSONData()).toStrictEqual(newTodo);
     });
+    it("should handle errors", async () => {
+        const errorMessage = { message: "Done property missing" };
+        const rejectedPromise = Promise.reject(errorMessage);
+        TodoModel.create.mockReturnValue(rejectedPromise);
+        await todoController.createTodo(req, res, next);
+        expect(next).toBeCalledWith(errorMessage);
+    })
 })
