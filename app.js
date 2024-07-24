9f0e4a2dd6eac8057dea03481dd5698e3e29b77b
luansantosdevpy <luansantosdev.py@gmail.com>
2024-07-24 12:05:57 -0300

    feat: add middleware and mongodb connect in application

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const todoRoutes = require("./routes/todo.routes");
 const app = express();
+const mongodb = require("./mongodb/mongodb.connect");
+
+mongodb.connect();
+
+app.use(express.json());
+
+app.use("/todos", todoRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
 
-app.listen(3000, () => {
-    console.log("Server is new running");
-});
+// app.listen(3000, () => {
+//     console.log("Server is new running");
+// });
+
+module.exports = app;
